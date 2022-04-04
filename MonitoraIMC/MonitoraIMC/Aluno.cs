51024e2ccdb1b9a4b314ba4cf3b8d1684db95091
bernyt <95032294+bernyt@users.noreply.github.com>
2022-04-04 01:53:29 -0300

    Add files via upload

---
+++ MonitoraIMC/MonitoraIMC/Aluno.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoraIMC
+{
+    class Aluno
+    {   
+        // Atributos
+        public string nome;
+        public int idade;
+        public float altura;
+        public float peso;
+
+        // Construtor
+        public Aluno(string nome_, int idade_, float altura_, float peso_)
+        {
+            nome = nome_;
+            idade = idade_;
+            altura = altura_;
+            peso = peso_;
+        }
+
+        // Método 1
+        public float CalcularIMC()
+        {
+            return peso / (altura * altura);
+        }
+
+        // Método 2
+        public string DescreverSituacaoCorporal()
+        {
+            string situacaoCorporal = "Não foi possível verificar a situação corporal!";
+            double imc = CalcularIMC();
+            if ((18.5 <= imc) && (24.9 >= imc))
+            {
+                situacaoCorporal = "Parabéns - você está em seu peso normal!";
+            }
+            if ((25.0 <= imc) && (29.9 >= imc))
+            {
+                situacaoCorporal = "Você está acima de seu peso (sobrepeso).";
+            }
+            if ((30.0 <= imc) && (34.9 >= imc))
+            {
+                situacaoCorporal = "Obesidade grau I.";
+            }
+            if ((35.0 <= imc) && (39.9 >= imc))
+            {
+                situacaoCorporal = "Obesidade grau II.";
+            }
+            if (imc >= 40)
+            {
+                situacaoCorporal = "Obesidade grau III e IV.";
+            }
+            return situacaoCorporal;
+        }
+
+        // Método 3
+        public float CalculareMetaPerdaPeso()
+        {
+            float imcMaxIdeal = 24.9f;
+            float pesoIdeal = imcMaxIdeal * (altura * altura);
+            if (peso - pesoIdeal <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return peso - pesoIdeal;
+            }
+        }
+    }
+}
---
+++ MonitoraIMC/MonitoraIMC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoraIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno alunoBob = new Aluno("Bob Nelson", 37, 1.70f, 78);
+            Console.WriteLine($"O IMC do(a) {alunoBob.nome} é: {alunoBob.CalcularIMC().ToString("F")} Kg/m2.");
+            Console.WriteLine($"A situação é: {alunoBob.DescreverSituacaoCorporal()}");
+            Console.WriteLine($"Meta de perda de peso: {alunoBob.CalculareMetaPerdaPeso().ToString("F")} kg.");
+            Console.ReadLine();
+
+            Aluno alunoTestolfo = new Aluno("Testolfo Rocha", 43, 1.65f, 60);
+            Console.WriteLine($"O IMC do(a) {alunoTestolfo.nome} é: {alunoTestolfo.CalcularIMC().ToString("F")} Kg/m2.");
+            Console.WriteLine($"A situação é: {alunoTestolfo.DescreverSituacaoCorporal()}");
+            Console.WriteLine($"Meta de perda de peso: {alunoTestolfo.CalculareMetaPerdaPeso().ToString("F")} kg.");
+            Console.ReadLine();
+
+            Aluno alunoLisa = new Aluno("Lisa Leite", 22, 1.72f, 92);
+            Console.WriteLine($"O IMC do(a) {alunoLisa.nome} é: {alunoLisa.CalcularIMC().ToString("F")} Kg/m2.");
+            Console.WriteLine($"A situação é: {alunoLisa.DescreverSituacaoCorporal()}");
+            Console.WriteLine($"Meta de perda de peso: {alunoLisa.CalculareMetaPerdaPeso().ToString("F")} kg.");
+            Console.ReadLine();
+        }
+    }
+}
